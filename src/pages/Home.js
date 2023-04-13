5a6e90a657808765fef9d28981d1c4487ee5863a
Edwin Soto <edwinsoto-villela@my.unt.edu>
2023-04-13 00:13:07 -0500

    Added more content to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,30 @@
-import React from "react"
-export const  Home = () => {
-    return  <div className="header"> 
-                <h1>Welcome to UNTRides</h1>
-                    <p>hi</p>
-            </div>
-}
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+      <main>
+        <section className="header">
+          <h1>Welcome to UNTRides!</h1>
+          <p>Join our community and share rides with ease.</p>
+          {/* Add your call to action button here */}
+          <button>Sign up</button>
+        </section>
+        <section className="bottom-section">
+          <h2>Why Choose UNTRides?</h2>
+          <ul>
+            <li>Convenient and affordable</li>
+            <li>Safe and reliable</li>
+            <li>Environmentally friendly</li>
+          </ul>
+        </section>
+      </main>
+      <footer>
+        {/* Add your footer content here */}
+        <p>Contact us: example@example.com</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
